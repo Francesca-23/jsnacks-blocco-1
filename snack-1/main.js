0eac2a939eaac424c4de9b9ef6c324b81438792f
Francesca-23 <francescadidomenico4@gmail.com>
2023-03-28 11:01:57 +0200

    primo snack

---
+++ snack-1/main.js
@@ -0,0 +1,8 @@
+let numero1 = prompt('inserisci un numero');
+let numero2 = prompt('inserisci un numero');
+
+if(numero1 > numero2){
+    document.writeln(numero1);
+}else{
+    document.writeln(numero2);
+}
